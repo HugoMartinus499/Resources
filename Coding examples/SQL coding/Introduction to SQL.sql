2462fba3007378fa9e1dcdaad2496191d70bd2ef
Hugo von Essen-Müller <hugo.von.essen@gmail.com>
2024-03-25 20:18:37 +0100

    CASE WHEN exercises completed

--- Coding examples/SQL coding/Introduction to SQL.sql
+++ Coding examples/SQL coding/Introduction to SQL.sql
@@ -657,3 +657,85 @@ SELECT DATE_TRUNC('month', o.occurred_at) ord_date, SUM(o.gloss_amt_usd) tot_spe
 	ORDER BY 2 DESC
 LIMIT 1;
 -- May 2016
+
+-- Unit price for each account, fix with CASE (earlier assignment)
+SELECT account_id, 
+	CASE WHEN standard_qty = 0 OR standard_qty IS NULL 
+    	THEN 0
+		ELSE standard_amt_usd/standard_qty 
+    END AS unit_price
+	FROM orders;
+
+-- Orders with account, level and amount
+SELECT account_id, total_amt_usd, 
+		CASE 
+   			WHEN total_amt_usd > 3000 THEN 'Large'
+   			ELSE 'Small' 
+            END AS order_level
+   	FROM orders;
+   
+-- Number of orders in 3 categories
+SELECT CASE 
+		WHEN total >= 2000 THEN 'At Least 2000'
+		WHEN total >= 1000 AND total < 2000 THEN 'Between 1000 and 2000'
+    	ELSE 'Less than 1000' 
+        END AS order_category,
+	COUNT(*) AS order_count
+	FROM orders
+	GROUP BY 1;
+
+-- Ranking customers based on spend
+SELECT a.name, SUM(total_amt_usd) total_spent, 
+        CASE 
+        	WHEN SUM(total_amt_usd) > 200000 THEN 'top'
+        	WHEN  SUM(total_amt_usd) > 100000 THEN 'middle'
+        	ELSE 'low' 
+            END AS customer_level
+	FROM orders o
+	JOIN accounts a
+		ON o.account_id = a.id 
+	GROUP BY a.name
+	ORDER BY 2 DESC;
+    
+-- Ranking customers in 2016 and 2017
+SELECT a.name, SUM(total_amt_usd) total_spent, 
+        CASE 
+        	WHEN SUM(total_amt_usd) > 200000 THEN 'top'
+        	WHEN  SUM(total_amt_usd) > 100000 THEN 'middle'
+        	ELSE 'low' 
+            END AS customer_level
+	FROM orders o
+	JOIN accounts a
+		ON o.account_id = a.id 
+    WHERE occurred_at > '2015-12-31'
+	GROUP BY a.name
+	ORDER BY 2 DESC;
+    
+-- Top performing sales reps based on orders
+SELECT s.name, COUNT(*) num_ords,
+		CASE 
+    		WHEN COUNT(*) > 200 THEN 'top'
+        	ELSE 'not' 
+        	END AS sales_rep_level
+	FROM orders o
+	JOIN accounts a
+		ON o.account_id = a.id 
+	JOIN sales_reps s
+		ON s.id = a.sales_rep_id
+	GROUP BY s.name
+	ORDER BY 2 DESC;
+    
+-- Top performing sales reps based on amount or orders
+SELECT s.name, COUNT(*) num_ords, SUM(o.total_amt_usd) total,
+		CASE 
+    		WHEN COUNT(*) > 200 OR SUM(o.total_amt_usd) > 750000 THEN 'top'
+            WHEN COUNT(*) > 150 OR SUM(o.total_amt_usd) > 500000 THEN 'middle'
+        	ELSE 'not' 
+        	END AS sales_rep_level
+	FROM orders o
+	JOIN accounts a
+		ON o.account_id = a.id 
+	JOIN sales_reps s
+		ON s.id = a.sales_rep_id
+	GROUP BY s.name
+	ORDER BY 3 DESC;
