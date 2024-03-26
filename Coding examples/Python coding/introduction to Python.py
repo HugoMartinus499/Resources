376241e2e57daf99bef6de1223286f036c9db8bc
Hugo von Essen-Müller <hugo.von.essen@gmail.com>
2024-03-26 21:20:36 +0100

    operator explanations

--- Coding examples/Python coding/introduction to Python.py
+++ Coding examples/Python coding/introduction to Python.py
@@ -1,2 +1,9 @@
 # Python is case sensitive
 # Spacing is important in python
+
+## Data types and operators
+# +, -, *, /
+# % is mod, which is the remainder after dividing
+# ** is Exponentiation
+# // divides as rounds to nearest integer
+# ^ is a bitwise operator for XOR, this is not needed for now, can be read about here: https://wiki.python.org/moin/BitwiseOperators
