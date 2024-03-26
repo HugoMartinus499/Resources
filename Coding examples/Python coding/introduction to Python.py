9cc19db9e73b5d734d4a4aee1c26d0c0ff5fbcfd
Hugo von Essen-Müller <hugo.von.essen@gmail.com>
2024-03-26 21:34:23 +0100

    Arithmetic calculations with operators

--- Coding examples/Python coding/introduction to Python.py
+++ Coding examples/Python coding/introduction to Python.py
@@ -7,3 +7,14 @@
 # ** is Exponentiation
 # // divides as rounds to nearest integer
 # ^ is a bitwise operator for XOR, this is not needed for now, can be read about here: https://wiki.python.org/moin/BitwiseOperators
+
+## Code with comments
+
+print((23+32+64)/3) # average electricity bill over 3 months
+
+print((9*7)+(5*7)) # Tiles needed for floor of 9x7 tiles and 5x7 tiles
+
+print((17*6)-98) # Tiles left over after buying 17 packs of 6 tiles
+
+
+
