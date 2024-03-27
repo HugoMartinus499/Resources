a8031847bf910cc4ede355afedbf0a0d58dcfc80
Hugo von Essen-Müller <hugo.von.essen@gmail.com>
2024-03-27 17:59:53 +0100

    Structure in the .py doc

--- Coding examples/Python coding/introduction to Python.py
+++ Coding examples/Python coding/introduction to Python.py
@@ -2,11 +2,19 @@
 # Spacing is important in python
 
 ## Data types and operators
-# +, -, *, /
-# % is mod, which is the remainder after dividing
-# ** is Exponentiation
-# // divides as rounds to nearest integer
-# ^ is a bitwise operator for XOR, this is not needed for now, can be read about here: https://wiki.python.org/moin/BitwiseOperators
+#Useful knowledge:
+    #Data types: Integers, Floats, Booleans, Strings
+    #Operators: Arithmetic, Assignment, Comparison, Logical
+    #Built-In Functions, Type Conversion
+    #Whitespace and Style Guidelines
+#Arithmetic operators
+    # +, -, *, /
+    # % is mod, which is the remainder after dividing
+    # ** is Exponentiation
+    # // divides as rounds to nearest integer
+    # ^ is a bitwise operator for XOR, this is not needed for now, can be read about here: https://wiki.python.org/moin/BitwiseOperators
+
+    
 
 ## Code with comments
 
@@ -15,6 +23,8 @@ print((23+32+64)/3) # average electricity bill over 3 months
 print((9*7)+(5*7)) # Tiles needed for floor of 9x7 tiles and 5x7 tiles
 
 print((17*6)-98) # Tiles left over after buying 17 packs of 6 tiles
+
+# Variables and assignment operators
 
 
 
