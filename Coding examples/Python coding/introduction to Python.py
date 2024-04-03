4c2243c4cd06ad9111ee3adcec3d8dbac872e13d
Hugo von Essen-Müller <hugo.von.essen@gmail.com>
2024-04-03 22:44:09 +0200

    String methods

--- Coding examples/Python coding/introduction to Python.py
+++ Coding examples/Python coding/introduction to Python.py
@@ -168,4 +168,62 @@ print(statement + " " + str(total_sales)) # Print statement + space + total sale
 weekly_sales = int(mon_sales) + int(tues_sales) + int(wed_sales) + int(thurs_sales) + int(fri_sales)
 weekly_sales = str(weekly_sales)  #convert the type back!!
 print("This week's total sales: " + weekly_sales)
-    
+ 
+## Methods
+    # Methods are related to functions
+    # Associated to specific types of objects, unlike functions
+    # Used with arguments
+    # The object is always the first argument
+ 
+# Browse the complete list of string methods at:
+# https://docs.python.org/3/library/stdtypes.html#string-methods
+# and try them out here
+
+print(13.37.islower())   # This returns an error, as it is a string method and 13.37 is a float
+
+# Write two lines of code below, each assigning a value to a variable
+Employee = "Hugo"
+Value = "250"
+
+# Now write a print statement using .format() to print out a sentence and the 
+#   values of both of the variables
+
+print("{} just won a deal worth {}$".format(Employee,Value))
+
+##Questions
+# What is the length of the string variable verse?
+# What is the index of the first occurrence of the word 'and' in verse?
+# What is the index of the last occurrence of the word 'you' in verse?
+# What is the count of occurrences of the word 'you' in the verse?
+
+verse = "If you can keep your head when all about you\n  Are losing theirs and blaming it on you,\nIf you can trust yourself when all men doubt you,\n  But make allowance for their doubting too;\nIf you can wait and not be tired by waiting,\n  Or being lied about, don’t deal in lies,\nOr being hated, don’t give way to hating,\n  And yet don’t look too good, nor talk too wise:"
+print(verse)
+
+# Use the appropriate functions and methods to answer the questions above
+# Bonus: practice using .format() to output your answers in descriptive messages!
+verse = "If you can keep your head when all about you\n  Are losing theirs and blaming it on you,\nIf you can trust yourself when all men doubt you,\n  But make allowance for their doubting too;\nIf you can wait and not be tired by waiting,\n  Or being lied about, don’t deal in lies,\nOr being hated, don’t give way to hating,\n  And yet don’t look too good, nor talk too wise:"
+print(verse, "\n")
+
+# What is the length of the string variable verse?
+print("Verse has a length of {} characters.".format(len(verse)))
+# What is the index of the first occurrence of the word 'and' in verse?
+print("The first occurence of the word 'and' occurs at the {}th index.".format(verse.find('and')))
+# What is the index of the last occurrence of the word 'you' in verse?
+print("The last occurence of the word 'you' occurs at the {}th index.".format(verse.rfind('you')))
+# What is the count of occurrences of the word 'you' in the verse?
+print("The word 'you' occurs {} times in the verse.".format(verse.count('you')))
+
+# Alternative solution
+verse = "If you can keep your head when all about you\n  Are losing theirs and blaming it on you,\nIf you can trust yourself when all men doubt you,\n  But make allowance for their doubting too;\nIf you can wait and not be tired by waiting,\n  Or being lied about, don’t deal in lies,\nOr being hated, don’t give way to hating,\n  And yet don’t look too good, nor talk too wise:"
+print(verse, "\n")
+
+message = "Verse has a length of {} characters.\nThe first occurence of the \
+word 'and' occurs at the {}th index.\nThe last occurence of the word 'you' \
+occurs at the {}th index.\nThe word 'you' occurs {} times in the verse."
+
+length = len(verse)
+first_idx = verse.find('and')
+last_idx = verse.rfind('you')
+count = verse.count('you')
+
+print(message.format(length, first_idx, last_idx, count))
