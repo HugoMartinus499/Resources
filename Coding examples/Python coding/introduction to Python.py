e1fc59498790a1f72f77714eb54fe1d6145d0bef
Hugo von Essen-Müller <hugo.von.essen@gmail.com>
2024-04-21 22:35:36 +0200

    Control flow + introduction to conditional statements

--- Coding examples/Python coding/introduction to Python.py
+++ Coding examples/Python coding/introduction to Python.py
@@ -495,3 +495,137 @@ print(sorted_keys[0])
 
 # find the element with the highest value in the list of keys
 print(sorted_keys[-1]) 
+
+## Control flow
+# An if statement is a conditional statement that runs or skips code based on whether a condition is true or false. Here's a simple example.
+    
+if phone_balance < 5:
+    phone_balance += 10
+    bank_balance -= 10
+    
+# Use comparison operators for conditional statements
+# In addition to the if clause, there are two other optional clauses often used with an if statement. For example:
+
+if season == 'spring':
+    print('plant the garden!')
+elif season == 'summer':
+    print('water the garden!')
+elif season == 'fall':
+    print('harvest the garden!')
+elif season == 'winter':
+    print('stay indoors!')
+else:
+    print('unrecognized season')
+    
+# Identation is important in python, because it encloses blocks of code. Like what is included in an if statement
+
+#First Example - try changing the value of phone_balance
+phone_balance = 10
+bank_balance = 50
+
+if phone_balance < 10:
+    phone_balance += 10
+    bank_balance -= 10
+
+print(phone_balance)
+print(bank_balance)
+
+#Second Example - try changing the value of number
+
+number = 145
+if number % 2 == 0:
+    print("Number " + str(number) + " is even.")
+else:
+    print("Number " + str(number) + " is odd.")
+
+#Third Example - try to change the value of age
+age = 35
+
+# Here are the age limits for bus fares
+free_up_to_age = 4
+child_up_to_age = 18
+senior_from_age = 65
+
+# These lines determine the bus fare prices
+concession_ticket = 1.25
+adult_ticket = 2.50
+
+# Here is the logic for bus fare prices
+if age <= free_up_to_age:
+    ticket_price = 0
+elif age <= child_up_to_age:
+    ticket_price = concession_ticket
+elif age >= senior_from_age:
+    ticket_price = concession_ticket
+else:
+    ticket_price = adult_ticket
+
+message = "Somebody who is {} years old will pay ${} to ride the bus.".format(age, ticket_price)
+print(message)
+
+# Write an if statement that lets a competitor know which of these prizes they won based on the number of points they scored, which is stored in the integer variable points.
+points = 174  # use this input to make your submission
+
+prize_name = ("wooden rabbit", "wafer-thin mint", "penguin")
+# write your if statement here
+if points <= 50:
+    result = "Congratulations! You won a {}!".format(prize_name[0])
+elif points <= 150:
+    result = "Oh dear, no prize this time."
+elif points <= 180:
+    result = "Congratulations! You won a {}!".format(prize_name[1])
+else:
+    result = "Congratulations! You won a {}!".format(prize_name[2])
+    
+
+print(result)
+
+# '''
+# You decide you want to play a game where you are hiding 
+# a number from someone.  Store this number in a variable 
+# called 'answer'.  Another user provides a number called
+# 'guess'.  By comparing guess to answer, you inform the user
+# if their guess is too high or too low.
+
+# Fill in the conditionals below to inform the user about how
+# their guess compares to the answer.
+# '''
+answer = 42
+guess = 69
+
+if guess < answer:
+    result = "Oops!  Your guess was too low."
+elif guess > answer:
+    result = "Oops!  Your guess was too high."
+elif guess == answer:
+    result = "Nice!  Your guess matched the answer!"
+
+print(result)
+
+# '''
+# Depending on where an individual is from we need to tax them 
+# appropriately.  The states of CA, MN, and 
+# NY have taxes of 7.5%, 9.5%, and 8.9% respectively.
+# Use this information to take the amount of a purchase and 
+# the corresponding state to assure that they are taxed by the right
+# amount.
+# '''
+state = "CA"
+purchase_amount = 69420
+
+if state == "CA":
+    tax_amount = .075
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+elif state == "MN":
+    tax_amount = .095
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+elif state == "NY":
+    tax_amount = .089
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+print(result)
