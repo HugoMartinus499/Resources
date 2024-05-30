ca72b99deb5c3f9723863f02e58ad44ceec9d205
Hugo von Essen-Müller <hugo.von.essen@gmail.com>
2024-05-30 22:34:23 +0200

    Palindrome faster solution

--- Coding examples/Leetcode/isPalindrome.py
+++ Coding examples/Leetcode/isPalindrome.py
@@ -1,6 +1,12 @@
+# My solution
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         x = str(x) # Convert int to string
         if x == x[::-1]: # The colons are separators. Rather than providing a "beginning" and an "end" index, it's telling Python to skip by every -1 objects in the array. It's effectively reversing the array.
             return True
         return False
+    
+# Alternative, faster solution
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1] # Compresses everything into one line
