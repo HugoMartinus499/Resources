f6de60db4f52d824a63ff83f689515a92fe911fe
HugoMartinus499 <hugo.von.essen@gmail.com>
2024-05-31 16:46:15 +0200

    Roman to integers

---
+++ Coding examples/Leetcode/romansTointegers.py
@@ -0,0 +1,28 @@
+# Solution using conditionals
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        ans = 0
+
+        for i in range(len(s)):
+            if i < len(s) - 1 and map[s[i]] < map[s[i+1]]:
+                ans -= map[s[i]]
+            else:
+                ans += map[s[i]]
+        
+        return ans
+    
+# Alternative solution
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        ans = 0
+
+        s = s.replace('IV', 'IIII').replace('IX', 'VIIII')
+        s = s.replace('XL', 'XXXX').replace('XC', 'LXXXX')
+        s = s.replace('CD', 'CCCC').replace('CM', 'DCCCC')
+
+        for i in s:
+                ans += map[i]
+        
+        return ans
